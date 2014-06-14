6515cf0bbb4e8bc6c3f63f89f57a70159f6566ae
Mark Schabacker <mark.schabacker@gmail.com>
2014-06-14 13:25:52 -0400

    Implement recursive solution with text quicklooks

--- swift/hanoi.playground/section-1.swift
+++ swift/hanoi.playground/section-1.swift
@@ -26,7 +26,7 @@ struct HanoiPuzzle {
     }
     
     init(numberOfDisks: Int){
-        var firstPeg = Array(1...numberOfDisks).map { HanoiDisk(size: $0) }
+        var firstPeg = Array(0..numberOfDisks).map { HanoiDisk(size: numberOfDisks - $0) }
         var secondPeg = Array<HanoiDisk>()
         var thirdPeg = Array<HanoiDisk>()
         
@@ -48,7 +48,31 @@ struct HanoiPuzzle {
         pegsCopy[fromPeg] = fromPegCopy
         pegsCopy[toPeg] = toPegCopy
         
-        return HanoiPuzzle(firstPeg: pegsCopy[HanoiPeg.First]!, secondPeg: pegsCopy[HanoiPeg.Second]!, thirdPeg: pegsCopy[HanoiPeg.Third]!)
+        let moveResults = HanoiPuzzle(firstPeg: pegsCopy[HanoiPeg.First]!, secondPeg: pegsCopy[HanoiPeg.Second]!, thirdPeg: pegsCopy[HanoiPeg.Third]!)
+        
+        HanoiPuzzleQuickLookHelper(puzzle: moveResults) // DEMO: put a quick look here
+        
+        return moveResults
+    }
+}
+
+class HanoiSolver : NSObject {
+    // TODO: track intermediate steps
+    func solve(puzzle: HanoiPuzzle) -> HanoiPuzzle {
+        var bottomDisk = puzzle.firstPeg[0]
+        return solveRec(puzzle, diskSize: bottomDisk.size, sourcePeg: HanoiPeg.First, destPeg: HanoiPeg.Third, tempPeg: HanoiPeg.Second)
+    }
+    
+    func solveRec(puzzle: HanoiPuzzle, diskSize: Int, sourcePeg: HanoiPeg, destPeg: HanoiPeg, tempPeg: HanoiPeg) -> HanoiPuzzle
+    {
+        if 1 == diskSize {
+            return puzzle.moveDiskFromPeg(sourcePeg, toPeg: destPeg)
+        }
+        else {
+            let smallerDisksMoved = solveRec(puzzle, diskSize: diskSize - 1, sourcePeg: sourcePeg, destPeg: tempPeg, tempPeg: destPeg)
+            let smallerDisksRemaining = smallerDisksMoved.moveDiskFromPeg(sourcePeg, toPeg: destPeg)
+            return solveRec(smallerDisksRemaining, diskSize: diskSize - 1, sourcePeg: tempPeg, destPeg: destPeg, tempPeg: sourcePeg)
+        }
     }
 }
 
@@ -67,14 +91,8 @@ class HanoiPuzzleQuickLookHelper : NSObject {
     }
 }
 
-let puzzle = HanoiPuzzle(numberOfDisks: 2)
-HanoiPuzzleQuickLookHelper(puzzle: puzzle)
 
-let firstStep = puzzle.moveDiskFromPeg(HanoiPeg.First, toPeg: HanoiPeg.Second)
-HanoiPuzzleQuickLookHelper(puzzle: firstStep)
-
-let secondStep = firstStep.moveDiskFromPeg(HanoiPeg.First, toPeg: HanoiPeg.Third)
-HanoiPuzzleQuickLookHelper(puzzle: secondStep)
-
-let thirdStep = secondStep.moveDiskFromPeg(HanoiPeg.Second, toPeg: HanoiPeg.Third)
-HanoiPuzzleQuickLookHelper(puzzle: thirdStep)
+let puzzle = HanoiPuzzle(numberOfDisks: 3)
+HanoiPuzzleQuickLookHelper(puzzle: puzzle) // DEMO: put a quick look here (Initial State)
+let result = HanoiSolver().solve(puzzle)
+HanoiPuzzleQuickLookHelper(puzzle: result) // DEMO: put a quick look here (Final State)
